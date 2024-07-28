4ecd5c4153cd4ad8d35ec4ef9c4ff4ce5319412d
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-07-28 21:30:15 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/node_fs98_front_YevhenKharchenko@e5f2333a78a0e6c1e08126ca7dcd1eeabf81c53b 🚀

---
+++ assets/NotFoundPage-ClXhx62i.js
@@ -0,0 +1,2 @@
+import{j as t,W as o}from"./index-S50c8m2H.js";const s="_notFoundWrapper_1b4r1_1",e="_text_1b4r1_10",r="_link_1b4r1_14",n={notFoundWrapper:s,text:e,link:r},c=()=>t.jsxs("div",{className:n.notFoundWrapper,children:[t.jsx("p",{className:n.text,children:"Page Not Found"}),t.jsx(o,{className:n.link,to:"/",children:"Go Home"})]});export{c as default};
+//# sourceMappingURL=NotFoundPage-ClXhx62i.js.map
